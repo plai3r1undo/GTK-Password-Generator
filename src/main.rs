070a1f73db84735210a77fcd8140799a75779158
plai3r1undo <90139041+plai3r1undo@users.noreply.github.com>
2023-08-19 15:44:56 +0200

    password generator function

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,38 @@
-
-use rand::*;
+use rand::{thread_rng, Rng};
 
 fn main() {
-
-
-    let pass = generate_password(7, true);
+    let _pass = generate_password(7, false);
+    println!("pass: {}", _pass);
 }
 
-
-
-
-
+ 
 fn generate_password(length: usize, numbers: bool) -> String {
+    const NUM: usize = 3;
     let mut rng = thread_rng();
-
     let alphabet_chars: &[u8] = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let num_chars: &[u8] = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    let only_num_chars: &[u8] = b"1234567890";
+    let char_set = if numbers { num_chars } else { alphabet_chars };
+    
 
+    let random_string: String = (0..length)
+        .map(|_| {
+            let index = rng.gen_range(0..char_set.len());
+            char_set[index] as char
+        })
+        .collect();
+    
     if numbers {
-        let random_string: String = (0..length)
-        .map(|_| {
-            let index = rng.gen_range(0..num_chars.len());
-            alphabet_chars[index] as char
+        let new_string: String= random_string[0..random_string.len() - NUM].to_owned();
+        let random_num: String = (0..NUM).map(|_| {
+            let index = rng.gen_range(0..only_num_chars.len());
+            only_num_chars[index] as char
         })
         .collect();
-        return random_string;
+        let password = format!("{}{}",new_string,random_num);
+        return password;
     } else {
-        let random_string: String = (0..length)
-        .map(|_| {
-            let index = rng.gen_range(0..num_chars.len());
-            alphabet_chars[index] as char
-        })
-        .collect();
         return random_string;
-
     }
 }
+
